771b85f71db8b3cdf3aae71730e7e0d5591ceda1
LErnandes <luis78270@gmail.com>
2022-04-21 04:03:42 -0300

    Adding commits

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import pandas as pd
 
+
+# 1. Remover 0 da frente de numeros
 df = pd.read_csv('titles.csv', sep=',', encoding='utf-8')
 print(df)
 
+# 2. Remover chaves e caracteres diversos do title
 
