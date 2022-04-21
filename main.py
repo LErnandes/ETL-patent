a38f020798aca723aba411f442e9a902633895de
LErnandes <luis78270@gmail.com>
2022-04-21 04:16:04 -0300

    Adding requirement 2

--- main.py
+++ main.py
@@ -3,7 +3,13 @@ import pandas as pd
 
 # 1. Remover 0 da frente de numeros
 df = pd.read_csv('titles.csv', sep=',', encoding='utf-8')
-print(df)
 
 # 2. Remover chaves e caracteres diversos do title
+for char in ['[', ']', '/', '{', '}', ';', ':', '(', ')', '...']:
+    df['title'] = df['title'].str.replace(char, '', regex=False)
 
+
+# 3. Converter numeros float para int em colunas de numeros inteiros
+
+
+print(df)
